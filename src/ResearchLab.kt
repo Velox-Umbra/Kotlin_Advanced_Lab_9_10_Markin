b7cfaf538319574bc5cae3a43aa83bc97ae7e393
Velox-Umbra <creatoris.semita@gmail.com>
2026-03-10 09:10:05 +0300

    Add abstract modules, interfaces and first module implementations

---
+++ src/ResearchLab.kt
@@ -0,0 +1,14 @@
+class ResearchLab : OutpostModule (
+    "Исследовательская лаборатория"
+) {
+    override fun performAction(manager: ResourceManager) {
+        val minerals = manager.get("Minerals")
+        if (minerals == null || minerals.amount < 30) {
+            println("Недостаточно материалов для исследования")
+            return
+        } else {
+            minerals.amount -= 30
+            println("Лаборатория проводит исследование (минералы -20)")
+        }
+    }
+}
